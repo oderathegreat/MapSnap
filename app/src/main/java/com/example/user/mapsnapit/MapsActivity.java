f44995195578361ee847c7956036353dd0095a58
oderathegreat <oderathegreat@gmail.com>
2018-08-26 11:00:42 +0300

    navigational drawer for dashboard activity

--- app/src/main/java/com/example/user/mapsnapit/MapsActivity.java
+++ app/src/main/java/com/example/user/mapsnapit/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.user.mapsnapit;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -10,6 +11,8 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,11 +28,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     LocationManager locationManager;
+    Button enter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        enter = findViewById(R.id.btnEnter);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Upon query go the next page
+
+                Intent intent = new Intent(MapsActivity.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
