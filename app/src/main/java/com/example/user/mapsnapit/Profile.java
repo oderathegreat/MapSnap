df1414646ba496bad083eeac6fdf494d994937e0
oderathegreat <oderathegreat@gmail.com>
2018-08-27 16:24:25 +0300

    profile user interface

---
+++ app/src/main/java/com/example/user/mapsnapit/Profile.java
@@ -0,0 +1,22 @@
+package com.example.user.mapsnapit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Profile extends AppCompatActivity {
+
+
+    Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
